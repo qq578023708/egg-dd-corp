3d6b4624d4bf3f38611804665da3235bbde0282e
bob <hongjian1122@sina.com>
2021-03-09 21:39:42 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -9,5 +9,7 @@ exports.dd = {
     custom: false,
     encodingAESKey: 'oyBrMEjoGjdoRvjiNWM1cvKZKNOZtsHEKS0BJwl23nd',
     token: 'fCDKpSJRUaGHr0vi',
+    robotSecret:'SECa429e9bebf96f98718d06f1c8ac0d984c95077c857501d55e24ddbfde7f37f37',
+    robotToken:'68baa1ef80831108b77175bb3c5a9c1ed1dc258e11d3bc6c4b7d53ff4fff6c8b',
   },
 };
